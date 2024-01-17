91836ec57b620c5a1f677c1155921386127dbcff
masudur0823 <masudur0823@gmail.com>
2024-01-17 13:24:15 +0600

    e

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import CaptureAndShare from "./components/CaptureAndShare";
-// import WhatsAppShare1 from "./components/WhatsAppShare1";
-// import WhatsAppShare2 from "./components/WhatsAppShare2";
 
 export default function App() {
   return (
     <>
       <CaptureAndShare />
-      {/* <WhatsAppShare2 /> */}
     </>
   );
 }
